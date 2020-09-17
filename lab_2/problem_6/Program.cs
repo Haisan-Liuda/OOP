d9b02a2300b62208e18f736c7dc6796832d245a9
Haisan-Liuda <haysan.lyudmyla@chnu.edu.ua>
2020-09-17 21:28:57 +0300

    6_zavd

---
+++ lab_2/problem_6/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+    class Problem_6
+    {
+        static void printArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine("\n");
+        }
+        static void Main()
+        {
+            int n;
+            int[] array;
+            Console.Write("Input size: ");
+            n = int.Parse(Console.ReadLine());
+            array = new int[n];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write("array[" + i + "]= ");
+                array[i] = int.Parse(Console.ReadLine());
+            }
+
+            printArray(array);
+
+            int count = 0, index = 0, max= 0;
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                count = 0;
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[i] == array[j])
+                    {
+                        count++;
+                    }
+                    else break;
+                }
+
+                if (count > max)
+                {
+                    max = count;
+                    index = i;
+                }
+            }
+            for (int i = index; i <= index + max; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine("\n");
+        }
+    }
+
