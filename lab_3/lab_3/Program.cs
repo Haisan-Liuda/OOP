ef3bf25211fbb960649b0d91450d2703dfb3346a
Haisan-Liuda <haysan.lyudmyla@chnu.edu.ua>
2020-09-21 18:28:52 +0300

    3_lab

---
+++ lab_3/lab_3/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Class_name
+{    
+     class Person
+    {
+     private string Name = string.Empty;
+     private int Age = 0;
+     public string name
+     {
+     get
+     {
+     return Name;
+     }
+     set
+     {
+     Name = value;
+     }
+     }
+     public int age
+     {
+     get
+     {
+     return Age;
+     }
+     set
+     {
+     Age = value;
+     }
+     }
+    }
+}
